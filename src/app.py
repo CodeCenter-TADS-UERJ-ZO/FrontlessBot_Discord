62ccccb74e2300900071ffca804f6516a6c26050
MiraiMindz <78761663+MiraiMindz@users.noreply.github.com>
2022-12-26 12:38:23 -0300

    added main app and made small changes

---
+++ src/app.py
@@ -0,0 +1,14 @@
+"""Frontless Bot
+
+Um bot de gerenciamento para o meu servidor do Discord.
+
+Autores: Mirai
+Data de Início: 26/12/2022"""
+
+
+def main() -> None:
+    """Main Function"""
+
+
+if __name__ == "__main__":
+    main()
